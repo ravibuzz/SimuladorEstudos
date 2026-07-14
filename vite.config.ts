43df9f3d3dd4b2f32a5809e37c734c2850f2aa65
ravibuzz <130081074+ravibuzz@users.noreply.github.com>
2026-07-13 23:36:12 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      root: './',
+      build: {
+        outDir: 'dist',
+        rollupOptions: {
+          input: './index.html'
+        }
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
